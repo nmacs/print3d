768fa399a3a849912d884cf82c86b3bffa102372
Max Neklyudov <macscomp@gmail.com>
2014-01-30 13:05:47 +0400

    Make PS_ON pin invertable

--- firmware/app/pinio.c
+++ firmware/app/pinio.c
@@ -10,10 +10,16 @@ static char ps_is_on = 0;
 volatile uint8_t	psu_timeout = 0;
 
 void power_on() {
-
 	if (ps_is_on == 0) {
+		#ifdef POWER_LED_PIN
+			WRITE(POWER_LED_PIN, 1);
+		#endif
 		#ifdef	PS_ON_PIN
+		#ifdef PS_INVERT_ON
+			WRITE(PS_ON_PIN, 1);
+		#else
 			WRITE(PS_ON_PIN, 0);
+		#endif
 			SET_OUTPUT(PS_ON_PIN);
 			delay_ms(500);
 		#endif
@@ -28,15 +34,22 @@ void power_on() {
 }
 
 void power_off() {
-
 	stepper_disable();
 	x_disable();
 	y_disable();
 	z_disable();
 	e_disable();
+	
+	#ifdef POWER_LED_PIN
+		WRITE(POWER_LED_PIN, 0);
+	#endif
 
 	#ifdef	PS_ON_PIN
-		SET_INPUT(PS_ON_PIN);
+	#ifdef PS_INVERT_ON
+		WRITE(PS_ON_PIN, 0);
+	#else
+		WRITE(PS_ON_PIN, 1);
+	#endif
 	#endif
 
   #ifdef PS_MOSFET_PIN
